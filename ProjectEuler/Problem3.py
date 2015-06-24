83b9f94424c2605879be6e17e4dcded3073c2540
futilitycloset <liz@LizWinterssMBP.wireless.yale.internal>
2015-06-24 14:54:03 -0400

    Changed when it appends to primelist

--- ProjectEuler/Problem3.py
+++ ProjectEuler/Problem3.py
@@ -2,20 +2,21 @@
 
 #What is the largest prime factor of the number 600851475143 ?
 
-number = 39
+number = 600851475143
 divisor = 3
-primelist = [3]
+primelist = [3, 5, ]
 primedivisorlist = []
 
 while (divisor < number/2):
     for i in primelist:
-        if divisor%i != 0:
-            primelist.append(divisor)
+        if divisor%i == 0:
             break
+    else:
+        primelist.append(divisor)
     divisor = divisor + 2
 
 for i in primelist:
     if number%i == 0:
         primedivisorlist.append(i)
     
-print primelist
+print primedivisorlist[-1]
