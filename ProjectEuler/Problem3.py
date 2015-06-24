1f0ad3f25c7a3da47582180e452c85227c7bd001
futilitycloset <liz@LizWinterssMBP.wireless.yale.internal>
2015-06-24 14:37:42 -0400

    Revert "Made it stop appending each unsuccessful attempt at dividing by a prime to primmest"
    This reverts commit 0ce8cf35d5e72988014c0b314420996e8cff3635.

--- ProjectEuler/Problem3.py
+++ ProjectEuler/Problem3.py
@@ -2,20 +2,21 @@
 
 #What is the largest prime factor of the number 600851475143 ?
 
-number = 39
-divisor = 3
-primelist = [3]
+number = 600851475143
+divisor = 1
+primelist = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
 primedivisorlist = []
 
 while (divisor < number/2):
     for i in primelist:
-        if divisor%i != 0:
-            primelist.append(divisor)
+        if divisor%i == 0:
             break
+        else:
+            primelist.append(divisor)
     divisor = divisor + 2
 
 for i in primelist:
     if number%i == 0:
         primedivisorlist.append(i)
     
-print primelist
+print primedivisorlist[-1]
