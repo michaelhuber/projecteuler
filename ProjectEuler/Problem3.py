6d290c60a8369be488c594d362a61894121779f4
futilitycloset <liz@LizWinterssMBP.wireless.yale.internal>
2015-06-24 13:01:25 -0400

    First try at #3

--- ProjectEuler/Problem3.py
+++ ProjectEuler/Problem3.py
@@ -3,6 +3,19 @@
 #What is the largest prime factor of the number 600851475143 ?
 
 number = 600851475143
-while number>0:
+divisor = (number - 1) / 2
+factorlist = []
+def step(start, end , step):
+    while start <= end:
+        yield start
+        start = start - step
+    
+divisor.step((number - 1)/2, 1, 2)
+
+#while (divisor > 0):
+#    if number%divisor == 0:
+#        factorlist.append(divisor)
+#    divisor = divisor - 2
+#print factorlist
     
     
