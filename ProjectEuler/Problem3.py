5eedff9c6e9b56c6908c60eb74a2f01abab0476c
futilitycloset <liz@LizWinterssMBP.wireless.yale.internal>
2015-06-24 13:12:59 -0400

    Making a list of primes, then stepping through it

--- ProjectEuler/Problem3.py
+++ ProjectEuler/Problem3.py
@@ -3,19 +3,20 @@
 #What is the largest prime factor of the number 600851475143 ?
 
 number = 600851475143
-divisor = (number - 1) / 2
-factorlist = []
-def step(start, end , step):
-    while start <= end:
-        yield start
-        start = start - step
-    
-divisor.step((number - 1)/2, 1, 2)
+divisor = 1
+primelist = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+primedivisorlist = []
 
-#while (divisor > 0):
-#    if number%divisor == 0:
-#        factorlist.append(divisor)
-#    divisor = divisor - 2
-#print factorlist
+while (divisor < number/2):
+    for i in primelist:
+        if divisor%i == 0:
+            break
+        else:
+            primelist.append(divisor)
+    divisor = divisor + 2
+
+for i in primelist:
+    if number%i == 0:
+        primedivisorlist.append(i)
     
-    
+print primedivisorlist[-1]
