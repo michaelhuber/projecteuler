7ee4ed28f612ab48f0f288b44f3fad960fa17b3f
futilitycloset <liz@LizWinterssMBP.wireless.yale.internal>
2015-06-25 13:28:28 -0400

    Now it finds factors up to square root, finds primes up to square root, then determines if factors are divisible by primes.

--- ProjectEuler/Problem3.py
+++ ProjectEuler/Problem3.py
@@ -3,16 +3,51 @@
 #What is the largest prime factor of the number 600851475143 ?
 import math
 
-number = 2000
-divisor = 3
+number = 600851475143
+endpoint = int(math.sqrt(number)) + 1
+lowdivisorlist = []
+highdivisorlist = []
+possibleprime = 3
 primelist = [2]
 primedivisorlist = []
 allfailed = True
 
-while divisor < math.sqrt(number):
+
+for i in range(2, endpoint):
+    if number%i == 0:
+        lowdivisorlist.append(i)
+        
+for i in lowdivisorlist:
+    highdivisorlist.append(number/i)
+
+divisorlist = lowdivisorlist + highdivisorlist
+divisorlist.sort()
+#print "The divisors of " + str(number) + " are " + str(divisorlist)
+
+while possibleprime < endpoint:
+    for prime in primelist:
+        if possibleprime%prime == 0:
+            allfailed = False
+    if allfailed == True:
+        primelist.append(possibleprime)
+    possibleprime = possibleprime + 2
+    allfailed = True
+
+for divisor in divisorlist:
+    for prime in primelist:
+        if divisor%prime == 0:
+            allfailed = False
+    if allfailed == True:
+        primedivisorlist.append(divisor)
+    allfailed = True
+
+print primedivisorlist
+
+"""while divisor < math.sqrt(number):
     for i in primelist:
         if divisor%i == 0:
             allfailed == False
+            break  
     if allfailed == True:
         primelist.append(divisor)
     divisor = divisor + 2
@@ -25,4 +60,4 @@ importantprime = primedivisorlist[-1]
 answer = number/importantprime
 print primelist
 print answer
-print importantprime
+print importantprime"""
