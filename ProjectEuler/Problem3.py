2f91538761540ccf5d05ad435fb04f2637d68ef9
futilitycloset <liz@LizWinterssMBP.wireless.yale.internal>
2015-06-25 11:45:56 -0400

    Added bool to check if all of the primelist was failed

--- ProjectEuler/Problem3.py
+++ ProjectEuler/Problem3.py
@@ -3,16 +3,17 @@
 #What is the largest prime factor of the number 600851475143 ?
 import math
 
-number = 600851475143
+number = 2000
 divisor = 3
 primelist = [2]
 primedivisorlist = []
+allfailed = True
 
-while (divisor < math.sqrt(number)):
+while divisor < math.sqrt(number):
     for i in primelist:
         if divisor%i == 0:
-            break
-    else:
+            allfailed == False
+    if allfailed == True:
         primelist.append(divisor)
     divisor = divisor + 2
 
@@ -22,4 +23,6 @@ for i in primelist:
     
 importantprime = primedivisorlist[-1]
 answer = number/importantprime
+print primelist
 print answer
+print importantprime
