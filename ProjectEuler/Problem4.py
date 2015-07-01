9903b099de62c5f045592896a566e46e537a61af
futilitycloset <liz@LizWinterssMBP.wireless.yale.internal>
2015-07-01 12:56:02 -0400

    Starting problem 4

---
+++ ProjectEuler/Problem4.py
@@ -0,0 +1,27 @@
+#A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+#Find the largest palindrome made from the product of two 3-digit numbers.
+import math
+
+number = 998001
+endpoint = int(math.sqrt(number)) + 1
+lowdivisorlist = []
+highdivisorlist = []
+
+def factor(input):
+    endpoint = int(math.sqrt(number)) + 1
+    lowdivisorlist = []
+    highdivisorlist = []
+
+    for i in range(2, endpoint):
+        if input%i == 0:
+            lowdivisorlist.append(i)
+                
+    for i in lowdivisorlist:
+        highdivisorlist.append(number/i)
+
+    divisorlist = lowdivisorlist + highdivisorlist
+    divisorlist.sort()
+    print divisorlist
+
+factor(number)
